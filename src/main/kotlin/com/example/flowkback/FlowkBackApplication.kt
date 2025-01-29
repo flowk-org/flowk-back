02eb054bdf8fae33682c34ce64b910b888283622
YurDuiachenko <d.yurok22@mail.ru>
2025-01-29 18:57:07 +0300

    init

---
+++ src/main/kotlin/com/example/flowkback/FlowkBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flowkback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowkBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlowkBackApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/flowkback/FlowkBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flowkback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowkBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
